d28f6160d43271c81debd4735ea21d7039c4b888
ilyadegtyarev <36071533+ilyadegtyarev@users.noreply.github.com>
2018-02-07 17:19:32 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+// write your code here
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите значение: ");
+
+        for (int x = in.nextInt();x>0 ; x--)
+            if ((x%2)==0)
+                System.out.print(x + "; ");
+
+    }
+
+}
